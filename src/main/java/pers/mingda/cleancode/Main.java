ae16944ac60c2b42bdb7d5822d189f299c24962f
芮铭达 <mingdarui@gmail.com>
2020-05-12 00:55:10 +0100

    Main

--- src/main/java/pers/mingda/cleancode/Main.java
+++ src/main/java/pers/mingda/cleancode/Main.java
@@ -1,5 +1,22 @@
 package pers.mingda.cleancode;
 
 public class Main {
+    public static void main(String ... args) {
+        try {
+            Args arg = new Args("l,p#,d*",args);
+            boolean logging = arg.getBoolean('l');
+            int port = arg.getInt('p');
+            String directory = arg.getString('d');
+            executeApplication(logging, port, directory);
+        } catch (ArgsException e) {
+            System.out.printf("Argument error: %s\n", e.errorMessage());
 
+        }
+    }
+
+    private static void executeApplication(boolean logging, int port, String directory) {
+        System.out.println("Executing program with loggin: " + logging + ", " +
+                "port: " + port + ", and" +
+                "directory: " + directory);
+    }
 }
