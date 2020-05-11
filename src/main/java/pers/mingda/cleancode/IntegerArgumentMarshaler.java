f5af1831377b7f70191b0b80e027ac6ad1e23e66
芮铭达 <mingdarui@gmail.com>
2020-05-12 00:57:04 +0100

    IntegerArgumentMarshaler

---
+++ src/main/java/pers/mingda/cleancode/IntegerArgumentMarshaler.java
@@ -0,0 +1,14 @@
+package pers.mingda.cleancode;
+
+import java.util.Iterator;
+
+public class IntegerArgumentMarshaler implements ArgumentMarshaler {
+    public static int getValue(ArgumentMarshaler argumentMarshaler) {
+        return 0;
+    }
+
+    @Override
+    public void set(Iterator<String> currentArgument) throws ArgsException {
+
+    }
+}
