4262b78002bf30206985be3774341b93e39bba88
Mingda Rui <mingdarui@gmail.com>
2020-05-13 23:26:23 +0100

    BooleanArgumentMarshaler implementation

--- src/main/java/pers/mingda/cleancode/BooleanArgumentMarshaler.java
+++ src/main/java/pers/mingda/cleancode/BooleanArgumentMarshaler.java
@@ -3,12 +3,17 @@ package pers.mingda.cleancode;
 import java.util.Iterator;
 
 public class BooleanArgumentMarshaler implements ArgumentMarshaler {
-    public static boolean getValue(ArgumentMarshaler argumentMarshaler) {
-        return false;
-    }
+    private boolean booleanValue = false;
 
     @Override
     public void set(Iterator<String> currentArgument) throws ArgsException {
+        booleanValue = true;
+    }
 
+    public static boolean getValue(ArgumentMarshaler am) {
+        if (am instanceof BooleanArgumentMarshaler)
+            return ((BooleanArgumentMarshaler) am).booleanValue;
+        else
+            return false;
     }
 }
