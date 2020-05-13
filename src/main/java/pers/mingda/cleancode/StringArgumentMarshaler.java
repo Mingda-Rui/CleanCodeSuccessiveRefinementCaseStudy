b4db34cdc4293c8808f7372624395cd6db805d46
Mingda Rui <mingdarui@gmail.com>
2020-05-13 23:44:06 +0100

    SringArgumentMarshaler implementation

--- src/main/java/pers/mingda/cleancode/StringArgumentMarshaler.java
+++ src/main/java/pers/mingda/cleancode/StringArgumentMarshaler.java
@@ -1,14 +1,26 @@
 package pers.mingda.cleancode;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static pers.mingda.cleancode.ArgsException.ErrorCode.MISSING_STRING;
 
 public class StringArgumentMarshaler implements ArgumentMarshaler {
-    public static String getValue(ArgumentMarshaler argumentMarshaler) {
-        return "";
-    }
+    private String stringValue = "";
 
     @Override
     public void set(Iterator<String> currentArgument) throws ArgsException {
+        try {
+            stringValue = currentArgument.next();
+        } catch (NoSuchElementException e) {
+            throw new ArgsException(MISSING_STRING);
+        }
+    }
 
+    public static String getValue(ArgumentMarshaler am) {
+        if (am instanceof StringArgumentMarshaler)
+            return ((StringArgumentMarshaler) am).stringValue;
+        else
+            return "";
     }
 }
